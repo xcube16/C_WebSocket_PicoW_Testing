cdf48278b942ad7e7ed483b651a62c1a8f46a778
xcube <xcube16@gmail.com>
2024-07-27 18:38:46 -0500

    This new I/O task resumer thingy should work!

--- io_lock.c
+++ io_lock.c
@@ -3,17 +3,28 @@
 
 #include "pico/mutex.h"
 
+
+#define IOL_YIELD_REASON_END 0
+
 // As mutex resources are limited, just have one global mutex to protect the
 // lock/unlock mechanisms from multi-core and/or interrupt speggetti.
 mutex_t global_core_lock;
 
 
-typedef struct io_lock_obj_t {
+typedef struct iol_lock_obj_t {
     // The task that is waiting to process an I/O operation
     sub_task* waiting_task;
-    void* io_object;
-    uint8_t locked;
-} io_lock_obj;
+
+    bool (*check_reason)(size_t reason);
+
+    // What is the task waiting for?
+    void* waiting_reason;
+
+    // Protect the task from being continued while it is already running
+    // This would result in upside-down-world! Don't go to upside-down-world;
+    // don't continue an already running task.
+    bool locked;
+} iol_lock_obj;
 
 /**
  * @brief Just an ugly global init function for global variables of horable globalness
@@ -26,29 +37,90 @@ int iol_init() {
     return 0;
 }
 
-/**
- * @brief Tries to aquire the lock
- *
- * @param lock
- * @return true
- * @return false
- */
-bool iol_trylock(io_lock_obj* lock) {
-    bool locked = false;
+void iol_unlock(iol_lock_obj* lock) {
+    // We should be safe as this function only gets called when the task has ended
+    // Aka, a locked context basically.
+    lock->locked = false;
+}
 
-    //
+bool iol_trylock(iol_lock_obj* lock) {
+    bool locked = false;
     if (mutex_try_enter(&global_core_lock, NULL)) {
         if (!lock->locked) {
             lock->locked = locked = true;
         }
         mutex_exit(&global_core_lock);
     }
-
     return locked;
 }
 
-void iol_unlock(io_lock_obj* lock) {
-    // We should be safe as this function only gets called when the task has ended
-    // Aka, a locked context basically.
+void iol_continue(iol_lock_obj* lock) {
+    while (true) {
+
+        if (!iol_trylock(lock)) {
+            // did not aquire the lock. The task is still running and
+            // will do a reason check by its self when its ready.
+            break;
+        }
+
+        lock->waiting_reason = sub_task_continue(lock->waiting_task, 0/*no error*/);
+
+        iol_unlock(lock);
+
+        // Interrupts and other cores can now trigger notifications again.
+        // Check and handle any we might have missed while locked.
+
+        if (!lock->check_reason(lock->waiting_reason)) {
+            // Still waiting for a valid reason to resume or the task has ended.
+            break;
+        }
+    }
+}
+
+/**
+ * @brief Tries to continue a task if it was waiting for the specified reason to continue.
+ *
+ * @param lock
+ * @param reason The task might be waiting for this reason
+ * @return true
+ * @return false
+ */
+int iol_notify(iol_lock_obj* lock, size_t reason) {
+    if (lock->waiting_reason == reason) {
+        return 1;
+    }
+
+    iol_continue(lock);
+}
+
+/**
+ * @brief Initialize the lock object and run the task with args
+ *
+ * @param lock
+ * @return int
+ */
+int iol_task_run(iol_lock_obj* lock, bool (*check_reason)(size_t reason), sub_task* task, void* (*task_function)(sub_task*, void*), void* args) {
+    lock->waiting_task = task;
+    lock->check_reason = check_reason;
+    lock->waiting_reason = 0;
     lock->locked = false;
+
+    if (!iol_trylock(lock)) {
+        // This should not happen. Something is very wrong.
+        return 1;
+    }
+
+    lock->waiting_reason = sub_task_run(task, task_function, args);
+
+    iol_unlock(lock);
+
+    // Interrupts and other cores can now trigger notifications again.
+    // Check and handle any we might have missed while locked.
+
+    if (lock->check_reason(lock->waiting_reason)) {
+        // An interrupt must have came in just as the task was finishing.
+        iol_continue(lock);
+    }
+
+    return 0;
 }
